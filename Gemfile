3a038b5b5b019fa66da1e58ea69e800e9ce613db
Virginie Allflatt <vallflatt@gmail.com>
2023-02-22 16:21:03 +0100

    routes and controllers done

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem "jbuilder"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
+gem "simple_form", github: "heartcombo/simple_form"
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
@@ -61,3 +63,7 @@ group :development do
   # gem "spring"
 end
 
+gem 'rspec-rails', group: [ :test ]
+gem 'rails-controller-testing', group: [ :test ]
+gem 'rspec-rails', group: [ :test ]
+gem 'rails-controller-testing', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+bossette = {name: "La Bossette", address: "route de Berne 23, 1008 Lausanne", category: "french"}
+dieci = {name: "Dieci", address: "ch des bossons 31, 1018 Lausanne", category: "belgian"}
+hong = {name: "Hong", address: "route des fleurettes 21, 1007 Lausanne", category: "chinese"}
+
+[dishoom, pizza_east, bossette, dieci, hong].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
